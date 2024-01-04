a8197498b51a2d95ba1a5702de9dafb94ce4d760
CTII <clay.thompson13@gmail.com>
2024-01-04 02:30:16 -0500

    🔧🐍 Update User class to Profile in user/models.py in CCWebApp
    - Define __str__()
    - Define save() - save profile and including user profile photo.
    - Import PIL (pillow) - added in CCWebAppVenv.

--- users/models.py
+++ users/models.py
@@ -1,8 +1,40 @@
+""" Defines models for managing user data.
+This module utilizes Django's ORM to create and interact with user-related data in the database.
+Key classes:
+    - UserProfile: Represents a user in the system.
+Dependencies:
+    - Django (models module)
+"""
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
-class User(models.Model):
+class Profile(models.Model):
+    """
+    Represents a user in the system.
+
+    Attributes:
+        - firstname (str): The user's first name.
+        - lastname (str): The user's last name.
+        - phone (int, optional): The user's phone number.
+        - joined_date (date, optional): The date the user joined.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+
+    def __str__(self) -> str:
+        return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.save(self.image.path)
