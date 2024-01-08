386c749e7de9f8f011b6ca6f81841d504448f015
CTII <clay.thompson13@gmail.com>
2024-01-08 00:39:38 -0500

    🔧🐍 Add crispy forms to settings.py in CCWebApp.
    - Also set crispy template pack. 
    - Have no updated Django project to reflect changes due to issues with Inventory app.

--- CCWebApp/settings.py
+++ CCWebApp/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
     'inventory',
     'users',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
