f132e74439c99a292089293e8d4aa883e1efa480
CTII <clay.thompson13@gmail.com>
2024-01-08 00:43:00 -0500

    🔧🐍 Add doc strings to apps file in users in CCWebApp.
    - AppConfig and Ready.
    - Should I move users.signals import to top?

--- users/apps.py
+++ users/apps.py
@@ -2,7 +2,31 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    AppConfig for the 'users' app.
+
+    This AppConfig is responsible for configuring the 'users' app in a Django project.
+    It includes the necessary settings such as the app name and defines a 'ready' method,
+    which is called when the Django application is being initialized.
+
+    Attributes:
+        name (str): The name of the app, which is set to 'users'.
+    """
     name = 'users'
 
     def ready(self):
+         """
+        Method called when the Django application is being initialized.
+
+        This method is automatically called by Django during the startup process.
+        It is used here to import and activate signals defined in the 'users' app.
+
+        Signals are a way to allow decoupled applications to get notified when certain
+        actions occur elsewhere in the application.
+
+        Note:
+            The 'import users.signals' statement is included here to ensure that the
+            signals are registered when the 'users' app is ready.
+
+        """
         import users.signals
